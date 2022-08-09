7683ee56a6b2fa04ec7b0a055d6de2c0ee1abce9
johnfunston <j.funston@live.com>
2022-08-09 14:02:47 -0700

    Codewars Aug 9th, 2022

---
+++ 7kyu/testing123.js
@@ -0,0 +1,24 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+  let numberedArray = []
+  array.forEach((item, i) => numberedArray.push(`${i + 1}: ${item}`))
+  return numberedArray
+}
+
+//Revisited and refactored for brevity
+
+var number=function(array){
+    return array.map((value, i)=> {
+      return (`${i + 1}: ${value}`)
+    })
+  }
